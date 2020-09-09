6afddc1c65f7861559337ee03c321caba7884dd0
doryski <dominatorskyyy@gmail.com>
2020-09-09 16:28:33 +0200

    Updates

---
+++ precache-manifest.c8d69068f8759141a1e35db9afc82b60.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ad50ba5185241becf998095441ae9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11ff0c0ab22588cf244b",
+    "url": "/static/js/2.62a44310.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.62a44310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98e4fab2bdc99e80a0d3",
+    "url": "/static/js/3.6b746b47.chunk.js"
+  },
+  {
+    "revision": "0759cff50dd247a72115",
+    "url": "/static/js/4.a85a9924.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/4.a85a9924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0376c8596f7c3d290393",
+    "url": "/static/js/5.d81977a8.chunk.js"
+  },
+  {
+    "revision": "b4e1eb4df1a656d6664c",
+    "url": "/static/js/main.8d827ab9.chunk.js"
+  },
+  {
+    "revision": "570fcde4d09d75ad9ac7",
+    "url": "/static/js/runtime-main.beba767f.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "dfaa334008d1d142ff8d905638d5c2ea",
+    "url": "/static/media/PL_flag.dfaa3340.svg"
+  },
+  {
+    "revision": "988c55965f0fc943d9e38ed9954dbd62",
+    "url": "/static/media/UK_flag.988c5596.svg"
+  },
+  {
+    "revision": "fcca24b85600dc6159c8147b6596009c",
+    "url": "/static/media/bg_home.fcca24b8.png"
+  },
+  {
+    "revision": "77f0aa1d0a343d8802de9e42f39fc561",
+    "url": "/static/media/justjoinit.77f0aa1d.png"
+  },
+  {
+    "revision": "e777997ff39012703ed2493ec01f82ed",
+    "url": "/static/media/life_tracker_app.e777997f.png"
+  },
+  {
+    "revision": "647bcf23c74b8eba7117f8fb30d1a9aa",
+    "url": "/static/media/notebook_react_app.647bcf23.png"
+  },
+  {
+    "revision": "b44cfa02a5076348a21508c83385e944",
+    "url": "/static/media/weather_react_app.b44cfa02.png"
+  }
+]);
