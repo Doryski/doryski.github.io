0d14a9e364efabbbd196595ede5325794c007889
doryski <dominatorskyyy@gmail.com>
2020-11-25 21:59:23 +0100

    added formspree link

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,3 +1,4 @@
-export const PORTFOLIO_PATH = '/portfolio'
-export const CONTACT_PATH = '/contact'
+export const PORTFOLIO_PATH = '#portfolio'
+export const CONTACT_PATH = '#contact'
 export const EMAIL_REGEX = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+export const FORMSPREE_LINK = 'https://formspree.io/xvorpadg'
