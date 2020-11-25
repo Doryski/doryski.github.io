fbe4e5e5e503e7e6ef40cdadb4879af6cfcaf025
doryski <dominatorskyyy@gmail.com>
2020-11-25 21:56:27 +0100

    created Footere component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
+import { DiGithubBadge } from 'react-icons/di'
+import { GlobalContext } from '../../context'
 
 const Footer = () => {
-    return <Wrapper></Wrapper>
+    const { content } = useContext(GlobalContext)
+    return (
+        <Wrapper>
+            <Content>
+                {content.footer.createdBy}
+                <a
+                    style={{ marginLeft: '0.25em' }}
+                    href='https://github.com/Doryski'
+                    title='Github'
+                >
+                    <DiGithubBadge />
+                </a>
+                <a href='https://github.com/Doryski' title='Github'>
+                    Doryski
+                </a>
+            </Content>
+        </Wrapper>
+    )
 }
 
-export const Wrapper = styled.footer``
+export const Wrapper = styled.footer`
+    height: ${({ theme }) => theme.footer};
+    margin-top: 3em;
+`
 
+export const Content = styled.div`
+    width: 90%;
+    height: 100%;
+    margin: auto;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    @media only screen and (min-width: 400px) {
+        width: 80%;
+    }
+`
 export default Footer
