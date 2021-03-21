9f3ff80d74bd57128c1382413a81c8729973a9d2
Doryski <dominatorskyyy@gmail.com>
2021-03-21 12:41:28 +0100

    deleted portfolio and left only redirect link

--- src/components/shared/PageWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-const PageWrapper = styled.main`
-    min-height: 80vh;
-    background: transparent;
-`
-export default PageWrapper
--- src/helpers/getWindowWidth.ts
+++
@@ -1,6 +0,0 @@
-const getWindowWidth = () =>
-    window.innerWidth ||
-    document.documentElement.clientWidth ||
-    document.body.clientWidth
-
-export default getWindowWidth
